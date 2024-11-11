f398f9dcdd65e205b4606d7f4f309752d119a88e
soursawwy <dputrikss@gmail.com>
2024-11-11 14:58:30 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
   $("#cekAnagram").on("click", function(){
     let str1 = $("#string_1").val();
     let str2 = $("#string_2").val();
-    let sort1 = str1.split("").sort().join("");
-    let sort2 = str2.split("").sort().join("");
+    let sort1 = str1.toLowerCase().split("").sort().join("");
+    let sort2 = str2.toLowerCase().split("").sort().join("");
     if (sort1 === sort2) {
       $("#string_3").val("berhasil");
     } else {
@@ -15,7 +15,7 @@
   // Palindrome check
   $("#cekPalindrome").on("click", function(){
     let str = $("#string_palindrome").val();
-    let reversedStr = str.split("").reverse().join("");
+    let reversedStr = str.toLowerCase().split("").reverse().join("");
     if (str === reversedStr) {
       $("#hasil_palindrome").val("berhasil");
     } else {
